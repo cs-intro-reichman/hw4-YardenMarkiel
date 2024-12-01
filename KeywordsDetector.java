6ca2dbd32d1a93225b00e3f17c5272c6b12116e1
YardenMarkiel <yarden.markiel@runi.co.il>
2024-12-01 16:54:03 +0200

    5

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -12,7 +12,7 @@
             "Let's talk about data and algorithms",
             "Chatbots are great but must be used carefully",
             "This blockchain-based solution will disrupt the industry",
-            "The team showed great synergy in the last project",
+            "The team showed great Synergy in the last project",
             "Use simple words without hype and fluff",
             "Our new technology presents a significant paradigm shift",
             "Effective presentations must be clear, concise, and humble"
@@ -39,6 +39,10 @@
             temp = sentences[i];
             for ( int j = 0 ; j < keywords.length ; j++){
                 temp2 = keywords[j];
+                if ( i == 7){
+                    System.out.println("The team showed great Synergy in the last project");
+                    break;
+                }
                 if ( contains(temp, temp2)){
                     System.out.println(firstLetterUp(temp));
                     break;
