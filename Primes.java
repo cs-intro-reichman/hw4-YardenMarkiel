93dcd4040bee3c981a227d687276bd06b8d663b5
YardenMarkiel <yarden.markiel@runi.co.il>
2024-12-01 16:34:32 +0200

    fixed Prime

--- Primes.java
+++ Primes.java
@@ -3,14 +3,14 @@ public class Primes {
         double N = Integer.parseInt(args[0]);
         double counter = 0;
         int S = 2;
-        boolean arr[] = new boolean [(int)N] ;
+        boolean arr[] = new boolean [(int)N + 1] ;
         for ( int i = 0 ; i < arr.length ; i++){
             arr[i] = true;
         }
         arr[0] = false;
         arr[1] = false;
         while ( S <= Math.sqrt(N) ){
-            for ( int i = S ; i < N ; i = i + S ){
+            for ( int i = S ; i < N + 1 ; i = i + S ){
                 if ( (arr[i] == true) && ( i != S ) ) arr[i] = false; 
             } 
             S++;  
@@ -22,15 +22,7 @@ public class Primes {
                 counter++;
             }
         }
-        int tempCheck = 0;
-        int tempLength = arr.length;
-        for ( int i = 1 ; i < tempLength+1 ; i++){
-            if ( tempLength % i == 0) tempCheck++;
-        }
-        if ( tempCheck != 0) {
-            counter++;
-            System.out.println(tempLength);
-        }
+        
         double P = (counter / N) * 100;
         System.out.println( "There are " + (int)counter + " primes between 2 and " + (int)N + " (" + (int)P + "% are primes)" );
     }
