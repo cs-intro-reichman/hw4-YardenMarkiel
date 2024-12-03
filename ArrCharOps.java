a9c33d4b44b007674de1d379e62df21cc74015d8
YardenMarkiel <yarden.markiel@runi.co.il>
2024-12-03 13:04:20 +0200

    11

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -67,26 +67,18 @@ public class ArrCharOps {
         return j;
     }
 
-    public static int compareTo(String str1, String str2) {
-        if (str1.length() == 0 || str2.length() == 0) return -2; // Return -2 for empty strings
-    
-        int len1 = str1.length(); 
-        int len2 = str2.length(); 
-        int minLength = Math.min(len1, len2); // Minimum length to prevent out-of-bounds
-    
-        // Compare characters of both strings up to the minimum length
-        for (int i = 0; i < minLength; i++) {
-            if (str1.charAt(i) < str2.charAt(i)) return -1; // str1 is lexicographically smaller
-            if (str1.charAt(i) > str2.charAt(i)) return 1;  // str1 is lexicographically larger
+    public static char[] concat(char[] arr1, char[] arr2) {
+        if (arr1 == null || arr2 == null) return null;
+        char[] result = new char[arr1.length + arr2.length];
+        for (int i = 0; i < arr1.length; i++) {
+            result[i] = arr1[i];
         }
-    
-        // If all characters match but lengths differ, compare by length
-        if (len1 < len2) return -1;
-        if (len1 > len2) return 1;
-    
-        return 0; // Strings are lexicographically equal
+        for (int i = 0; i < arr2.length; i++) {
+            result[arr1.length + i] = arr2[i];
+        }
+        return result;
     }
-
+    
     public static char[] subArray(char[] arr, int beginIndex, int endIndex) {
         char[] newarr = new char[endIndex - beginIndex];
         if (newarr.length == 0) return null;
@@ -108,24 +100,20 @@ public class ArrCharOps {
     }
 
     public static int compareTo(String str1, String str2) {
-        if ((str1.length() == 0) || (str2.length() == 0)) return -2;
-        char[] arr1 = new char[str1.length()];
-        char[] arr2 = new char[str2.length()];
-        int N = arr1.length;
-        int J = 0;
-        for (int i = 0; i < arr1.length; i++) {
-            if ((charAt(arr1, i) < 'A') && (charAt(arr1, i) > 'z')) return -2;
+        int len1 = str1.length();
+        int len2 = str2.length();
+        int minLength = Math.min(len1, len2);
+
+        for (int i = 0; i < minLength; i++) {
+            char ch1 = str1.charAt(i);
+            char ch2 = str2.charAt(i);
+
+            if (ch1 != ch2) {
+                return ch1 - ch2; // Return positive or negative based on lexicographical order
+            }
         }
-        for (int i = 0; i < arr2.length; i++) {
-            if ((charAt(arr2, i) < 'A') && (charAt(arr1, i) > 'z')) return -2;
-        }
-        if (arr2.length < N) return 1;
-        else if (N != arr2.length) return -1;
-        for (int i = 0; i < N; i++) {
-            if (charAt(arr1, i) > charAt(arr2, J)) return 1;
-            else if (charAt(arr1, i) < charAt(arr2, J)) return -1;
-            J++;
-        }
-        return 0;
+
+        // If one string is longer than the other
+        return len1 - len2;
     }
 }
